cdc45b541da8e41760822086bab8cd014439c689
Erik Thorne <erik.j.thorne@gmail.com>
2015-12-15 15:59:36 -0800

    increased modal functionality
    added more configurations to modals - now you can have multiple on one
    page and even specify classes for custom styling

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,23 +1,30 @@
 $(document).ready(function(){
-	// var medEditor = new MediumEditor('.editable');
-	// $('.editable').mediumInsert({
-	// 	editor: medEditor,
-	// 	enabled: true,
-	// 	buttonLabels: 'fontawesome',
-	// 	addons: {
-	// 		images: false,
-	// 		embeds: false,
-	// 		priEntityEmbed: true
-	// 	}
-	// });
+	var medEditor = new MediumEditor('.editable');
+	$('.editable').mediumInsert({
+		editor: medEditor,
+		enabled: true,
+		buttonLabels: 'fontawesome',
+		addons: {
+			images: false,
+			embeds: false,
+			priEntityEmbed: {
+				
+			}
+		}
+	});
 
-	// $('#dump-content').click(function()
-	// {
-	// 	var allContents = medEditor.serialize();
-	// 	var elContent = allContents['element-0'].value;
+	$('#dump-content').click(function()
+	{
+		var allContents = medEditor.serialize();
+		var elContent = allContents['element-0'].value;
 
-	// 	$('#elContents').text(elContent);
-	// });
+		$('#elContents').text(elContent);
+	});
 
-	$('#open-modal').opensModal($('#modal').makeModal());
+	$('#modal').modal({
+		openTriggers:[{
+			element: $('#open-modal'),
+			openEvent: 'click'
+		}]
+	});
 });
--- src/js/modal.js
+++ src/js/modal.js
@@ -2,26 +2,71 @@
 	
 	'use strict';
 
-	$.fn.makeModal = function(){
-		var self = this;
-		
-		self.addClass('modal-content');
-		self.before('<div class="modal-backdrop"></div>');
-
-		return self;
+	var generateId = function () {
+		var seg = function()
+		{
+			return Math.floor((1 + Math.random()) * 0x10000)
+						.toString(16)
+						.substring(1);
+		}
+  		return seg() + seg() + '-' + seg() + '-' + seg() + '-' +
+				seg() + '-' + seg() + seg() + seg();
 	};
 
-	$.fn.opensModal = function(el)
-	{
+	$.fn.modal = function(options){
 		var self = this;
-		var clickFunc = function()
-		{
-			el.toggleClass('active');
-			$('.modal-backdrop').toggleClass('active');
-		};
 
-		self.click(clickFunc);
-		$('.modal-backdrop').click(clickFunc);
+		// style this class as a modal and
+		// add optional user specified styling
+		self.addClass('modal-content');
+		if (!!options.contentClass)
+		{
+			self.addClass(options.contentClass);
+		}
+
+		// add optional user specified styling to backdrop
+		var classes = 'modal-backdrop'; 
+		if (!!options.backdropClass)
+		{
+
+			if (options.backdropClass.constructor.toString().indexOf('Array') > -1)
+			{ 
+				for(var i = 0; i < options.backdropClass.length; i++)
+				{
+					classes += ' ' + options.backdropClass[i];
+				}
+			}
+		} 
+
+		// link back drop to this modal and
+		// give it expected functionality
+		var uniqueId = generateId();
+		self.before('<div id="' + uniqueId + '" class="' + classes + '"></div>');
+		self.backdropElement = $('#' + uniqueId);
+		self.backdropElement.click(function()
+		{
+			self.backdropElement.toggleClass('active');
+			self.toggleClass('active');
+		});
+
+		// register user specified triggers that open this modal
+		for(var i = 0; i < options.openTriggers.length; i++)
+		{
+			var trigger = options.openTriggers[i];
+
+			// TODO : register more events
+			switch(trigger.openEvent)
+			{
+				case 'click':
+				default:
+					trigger.element.click(function()
+					{
+						self.backdropElement.toggleClass('active');
+						self.toggleClass('active');
+					});
+					break;
+			}
+		}
 
 		return self;
 	};
