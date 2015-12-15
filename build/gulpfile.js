b754094e9d12d66277daebca353470b66000fae8
Erik Thorne <erik.j.thorne@gmail.com>
2015-12-15 14:19:27 -0800

    gulp-watch
    added gulp-watch npm package; configured gulp task to watch less files;
    watch now runs on default; renamed some variables for readability

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
-var gUtil = require('gulp-util');
-var gLess = require('gulp-less');
-var gUglify = require('gulp-uglify');
+var util = require('gulp-util');
+var less = require('gulp-less');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
 
 var basePath = '../'; // relative path to repository root
 var jsPath = basePath + 'src/js/';
@@ -11,8 +12,13 @@ var mainLessPath = lessPath + 'main.less';
 
 gulp.task('less', function(){
 	gulp.src(mainLessPath)
-		.pipe(gLess())
+		.pipe(less())
 		.pipe(gulp.dest(htmlPath));
 });
 
-gulp.task('default', ['less']);
+gulp.task('watch', function()
+{
+	gulp.watch(lessPath + '*.less', ['less']);
+});
+
+gulp.task('default', ['less', 'watch']);
