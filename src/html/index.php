f48b665a662ad08884463fd533cc5ff4bc14faf8
Erik Thorne <erik.j.thorne@gmail.com>
2015-12-30 18:05:15 -0800

    tweaks

--- src/html/index.php
+++ src/html/index.php
@@ -40,6 +40,7 @@
 			<?php include "modal/modal_main.php"; ?>
 		</div>
 
+		<!-- 
 		<button id="dump-from-editor" class="btn btn-primary">from editor</button>
 		<br />
 		<br />
@@ -53,6 +54,7 @@
 		<br />
 		<span>editor digestable content (html) created from server object by parser</span>
 		<div id="from-server"></pre>
+		!-->
 	</body>
 
 </html>
