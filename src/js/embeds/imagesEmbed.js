2400be601a96f25a7579362141d18f059c7d5b72
Erik Thorne <erik.j.thorne@gmail.com>
2015-12-31 13:28:13 -0800

    code cleanup

--- src/js/embeds/imagesEmbed.js
+++ src/js/embeds/imagesEmbed.js
@@ -28,15 +28,18 @@
 	}
 
 	function formatFileSize(bytes) {
-		if (typeof bytes !== 'number') {
+		if (typeof bytes !== 'number')
+		{
 			return '';
 		}
 
-		if (bytes >= 100000000) {
+		if (bytes >= 100000000)
+		{
 			return (bytes / 1000000000).toFixed(2) + ' GB';
 		}
 
-		if (bytes >= 1000000) {
+		if (bytes >= 1000000)
+		{
 			return (bytes / 1000000).toFixed(2) + ' MB';
 		}
 		return (bytes / 1000).toFixed(2) + ' KB';
