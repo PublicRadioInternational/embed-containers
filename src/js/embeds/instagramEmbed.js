a79d47e17ae15eac8c791c031c8c029aec410603
simonsn <simons.n@hotmail.com>
2016-02-07 17:09:19 -0800

    Create ability to embed instagram image

--- src/js/embeds/instagramEmbed.js
+++ src/js/embeds/instagramEmbed.js
@@ -34,7 +34,7 @@
 
 	instagramEmbed.prototype.getModelFromForm = function($el){
 		var self = this;
-		var formFields = $el.find('.form-control');
+		var formFields = $el.find('.embed-modal-form-control');
 		for(var i = 0; i < formFields.length; i++)
 		{
 			var name = formFields[i].name;
@@ -44,7 +44,7 @@
 				self.model[name] = value;
 			}
 		}
-		
+
 		var embedCodeName = 'EmbedCode';
 		var code = '<blockquote class="instagram-media" data-instgrm-captioned data-instgrm-version="6" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: auto; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px); ">' +
 						'<a href="' + 
@@ -77,7 +77,6 @@
 						'<span style="color:white">click here to show the toolbars</span>' +
 					'</div>' + 
 				'</div>';
-		code = $(".overlay").children().prop('disabled',true);
 
 		return code;
 	};
