fc9ec01a73d1a9200e842a77df8e2346ba2e3733
simonsn <simons.n@hotmail.com>
2016-02-09 14:00:24 -0800

    Added back erik's changes that didn't make it through my merge

--- src/js/embeds/relatedLinkEmbed.js
+++ src/js/embeds/relatedLinkEmbed.js
@@ -152,4 +152,62 @@
 >>>>>>> Moved RelatedLink autocomplete functionality into new function
 	};
 
+	relatedLinkEmbed.prototype.getModelFromForm = function($el)
+	{
+		var self = this;
+		self.parent.getModelFromForm($el);
+
+		var urlForms = $el.find('.related-link-url');
+		for(var i = 0; i < urlForms.length; i++)
+		{
+			self.model.links.push(formFields[i].value);
+		}
+	};
+
+	relatedLinkEmbed.prototype.populateFormWithModel = function($form)
+	{
+		var self = this;
+		self.parent.populateFormWithModel($form);
+
+		var linkClass = 'related-link-url';
+		var $linkList = $el.find('#related-link-list');
+		var $addLinkBtn = $el.find('#add-link-btn');
+
+		for(var i = 0; i < self.model.links.length; i++)
+		{
+			$addLinkBtn.click();	
+			$form.find('.' + linkClass).last().val(self.model.links[i]);
+		}
+	};
+
+	relatedLinkEmbed.prototype.initModal = function($el){
+		var self = this;
+
+		var linkClass = 'related-link-url';
+		var removeLinkClass = 'remove-link-btn';
+		var $linkList = $el.find('#related-link-list');
+		var $addLinkBtn = $el.find('#add-link-btn');
+
+		$addLinkBtn.click(function(){
+			var pseudoGuid = generateId();
+
+			$linkList.append(
+				'<div class="' + linkClass + '">' + 
+					'<div class="embed-modal-form">' +
+						'<input id="' + pseudoGuid' + " type="text" placeholder="link url" class="embed-modal-form-control">' +
+					'</div>' + 
+					'<button class="' + removeLinkClass + '">' + 
+						'<i class="fa fa-minus"></i>' + 
+					'</button>' + 
+				'</div>'
+			);
+
+			//self.initiAutoComplete($('#' + pseudoGuid)
+		});
+
+		$el.on('click', '.' + removeLinkClass, function(){
+			$(document.activeElement).closest('.' + linkClass).remove();
+		});
+	};
+
 })('', EntityEmbedTypes);
