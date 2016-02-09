af9dcdcb22054f13e41467ce4e6e8d511dd0e1bc
simonsn <simons.n@hotmail.com>
2016-02-09 14:17:39 -0800

    Pulled styling out of html and refactored based on Erik's comments

--- src/js/embeds/instagramEmbed.js
+++ src/js/embeds/instagramEmbed.js
@@ -34,16 +34,8 @@
 
 	instagramEmbed.prototype.getModelFromForm = function($el){
 		var self = this;
-		var formFields = $el.find('.embed-modal-form-control');
-		for(var i = 0; i < formFields.length; i++)
-		{
-			var name = formFields[i].name;
-			var value = formFields[i].value;
-			if (!!name && !!value)
-			{
-				self.model[name] = value;
-			}
-		}
+
+		self.parent.getModelFromForm($el);
 
 		var embedCodeName = 'EmbedCode';
 		var code = '<blockquote class="instagram-media" data-instgrm-captioned data-instgrm-version="6" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: auto; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px); ">' +
@@ -66,15 +58,15 @@
 	instagramEmbed.prototype.parseForEditor = function(){
 		var self = this;
 		
-		var code= '<div class="instagram-embed" style="background-color:gray">' +
+		var code= '<div class="instagram-embed">' +
 					'<div class="instagram-info">' +
-						'<span style="color:white">click here to show the toolbars</span>' +
+						'<span>click here to show the toolbars</span>' +
 					'</div>' + 
 					'<div class="overlay" disable="true">' +
 					self.model.EmbedCode + 
 					'</div>' +
 					'<div class="instagram-info">' +
-						'<span style="color:white">click here to show the toolbars</span>' +
+						'<span>click here to show the toolbars</span>' +
 					'</div>' + 
 				'</div>';
 
