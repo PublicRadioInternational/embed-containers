45d73ea8a6b17de679808e18768e4260f87ce3c8
JXVD <jackvangame@live.com>
2016-03-23 07:29:23 -0700

    Initial Changes
    File system slightly reworked in the PHP file.  Both Buttons do the same
    thing currently.  System currently works if both buttons are used to put
    files into modal.

--- src/html/uploadWAV.php
+++ src/html/uploadWAV.php
@@ -1,6 +1,6 @@
 <?php
-	$audioDir = 'contents/audio/';
-	$inputName = 'audioFile';
+	$audioDir = 'contents/audio/WAV/';
+	$inputName = 'wavFile';
 	
 	$status = '';
 	$error = '';
@@ -11,34 +11,26 @@
 		mkdir($audioDir);
 	}
 
-	$acceptableTypes = array('mp3', 'wav');
+	$acceptableTypes = array('wav');
 
 	if(isset($_FILES[$inputName]) && $_FILES[$inputName]['error'] == 0){
 		$fileName = $_FILES[$inputName]['name'];
 		$extension = pathinfo($fileName, PATHINFO_EXTENSION);
-		if(in_array(strtolower($extension), $acceptableTypes)){
-			
-			//make folder for file types if they don't exist yet
-			$newDir = $audioDir . $extension . '/';
-			if(!file_exists($newDir))
-			{
-				mkdir($newDir);
-			}
 
-			if(move_uploaded_file($_FILES[$inputName]['tmp_name'], $newDir . $fileName)){
-				$status = 'success';
-				$audioPath = $newDir . $fileName;
-			}
-			else
-			{
-				$status = 'failure';
-				$error = 'Could not move file: please verify that the file path specified is accurate.';			
-			}
-		}
-		else{
+		if(!in_array(strtolower($extension), $acceptableTypes)){
 			$status = 'failure';
 			$error = 'File type not permitted.';
-		}	
+			$audioPath = '';
+		}
+		elseif(move_uploaded_file($_FILES[$inputName]['tmp_name'], $audioDir . $fileName)){
+			$status = 'success';
+			$audioPath = $audioDir . $fileName;
+		}
+		else
+		{
+			$status = 'failure';
+			$error = 'Could not move file: please verify that the file path specified is accurate.';			
+		}
 	}
 	else
 	{
