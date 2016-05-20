088d10cc522510157ddf08df9890ea0187face1c
Rick Peterman <rpeterman@globalpost.com>
2016-05-20 13:41:24 -0400

    Updated dist files

--- dist/js/embed-containers.js
+++ dist/js/embed-containers.js
@@ -3663,12 +3663,6 @@ var EntityEmbed = EntityEmbed || {};
 
 		self.parent.getModelFromForm($el, self);
 
-		if (self.model.url.endsWith('/'))
-		{
-			self.model.url.substr(0, self.model.url.length - 1);
-		}
-		self.model.tweetId = self.model.url.substr(self.model.url.lastIndexOf('/') + 1);
-
 		self.model.embedCode =
 			'<blockquote class="twitter-tweet" data-lang="en">' +
 				'<a href="' + self.model.url + '"></a>' +
@@ -4326,19 +4320,32 @@ var EntityEmbed = EntityEmbed || {};
 		var self = this;
 		var editor = editor = self.core.getEditor();
 
+		function findTopParent($elm) {
+			var $parent = $elm.parent();
+			if($parent[0] !== self.el)
+			{
+				return findTopParent($parent);
+			}
+
+			return $elm;
+		}
+
 		editor.subscribe('editableInput', function(data, editableElm) {
 			var badMarkup = [
 				'p > ol',
 				'p > ul',
-				'p > p'
+				'p > p',
 			].join(',');
 			var $badMarkup = $(editableElm).find(badMarkup);
+			var $hasStyleAttr = $(editableElm).find('[style]');
 
 			$badMarkup.each(function() {
 				var $this = $(this);
 				$this.unwrap();
 				editor.selectElement(this);
-			})
+			});
+
+			$hasStyleAttr.removeAttr('style');
 		});
 
 		$(document)
@@ -4361,7 +4368,7 @@ var EntityEmbed = EntityEmbed || {};
 			})
 			// prevent user from destroying modal functionality when deleting first element
 			.on('keydown', function(e){
-				var selection, range, textLength, selectionLength, numChildren, isEmptyP, siblingIsEmbed, $anchor, $sibling, $base;
+				var selection, range, textLength, selectionLength, numChildren, isEmptyAnchor, siblingIsEmbed, $anchor, $sibling, $base;
 				var protectedElms = ['.entity-embed-container', '[contenteditable]'].join(',');
 				var notProtectedElms = ':not(' + protectedElms + ')';
 
@@ -4383,8 +4390,8 @@ var EntityEmbed = EntityEmbed || {};
 				selectionLength = range.endOffset - range.startOffset; // Get length of current selection
 				$anchor = $(selection.anchorNode); // Get the element the selection is currently originating from
 				textLength = $anchor.text().length;
-				numChildren = self.$el.children().not('.medium-insert-buttons').length; // Get number of editors children that are not UI fof MEIP
-				isEmptyP = false;
+				numChildren = self.$el.children().not('.medium-insert-buttons').length; // Get number of editors children that are not UI for MEIP
+				isEmptyAnchor = false;
 				siblingIsEmbed = false;
 
 				if (selectionLength > 0)
@@ -4394,13 +4401,16 @@ var EntityEmbed = EntityEmbed || {};
 					return;
 				}
 
+				// If anchor is a text node, query for closest usable parent
 				if($anchor[0].nodeType === 3)
 				{
 					$anchor = $anchor.closest(notProtectedElms);
 				}
 
+				$anchor = findTopParent($anchor);
+
 				// Check to see if our anchor element is a p tag with no text
-				isEmptyP = $anchor.is(notProtectedElms) && !$anchor.text().length;
+				isEmptyAnchor = $anchor.is(notProtectedElms) && !$anchor.text().length;
 
 				// Get the previous sibling when
 				// 	- Backspace is pressed
@@ -4430,13 +4440,14 @@ var EntityEmbed = EntityEmbed || {};
 
 				// Prevent default when:
 				// 	- Anchor is the last empty p tag
-				// 	- A sipling element was fond and is and embed
-				if ( (isEmptyP && numChildren <= 1) || siblingIsEmbed)
+				// 	- A sipling element was found and is and embed
+				if ( (isEmptyAnchor && numChildren <= 1) || siblingIsEmbed)
 				{
 					e.preventDefault();
 				}
 
-				if(isEmptyP && numChildren > 1)
+				//
+				if(isEmptyAnchor && numChildren > 1)
 				{
 					e.preventDefault();
 
@@ -4464,6 +4475,9 @@ var EntityEmbed = EntityEmbed || {};
 
 					// Remove empty anchor element
 					$anchor.remove();
+
+					// Trigger input event
+					self.core.triggerInput();
 				}
 
 			})
